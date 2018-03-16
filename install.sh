becf3afbcbe61742ce73e577aee36a080a9ccd59
Frank Ploss <mail@fqxp.de>
2018-03-16 17:15:12 +0100

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+for pkg_base in src/nete-common src/nete-backend src/nete-cli ; do
+    (
+        cd "$pkg_base" || exit 1
+        rm dist/*
+        ./setup.py sdist
+        /usr/bin/pip install --user --upgrade dist/*.tar.gz
+    )
+done
