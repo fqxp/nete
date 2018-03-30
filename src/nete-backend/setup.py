f571b57b5fe94524173a47b4f07df71ab034a2db
Frank Ploss <mail@fqxp.de>
2018-03-30 12:46:39 +0200

    Version 0.1

--- src/nete-backend/setup.py
+++ src/nete-backend/setup.py
@@ -44,7 +44,7 @@ setup(
         ],
     },
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.5',
