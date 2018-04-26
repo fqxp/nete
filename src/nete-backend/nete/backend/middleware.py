698d47445261a94ae5ab10173da41159068d3df9
Frank Ploss <mail@fqxp.de>
2018-04-26 06:36:22 +0200

    Improve logging during tests

--- src/nete-backend/nete/backend/middleware.py
+++ src/nete-backend/nete/backend/middleware.py
@@ -3,6 +3,7 @@ from aiohttp import web
 from marshmallow.exceptions import ValidationError
 import json
 import logging
+import traceback
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +29,13 @@ async def error_middleware(request, handler):
         return await handler(request)
     except ValidationError as e:
         logger.error(e)
+        logger.error(traceback.format_exc())
         raise web.HTTPUnprocessableEntity(body=str(e))
     except json.decoder.JSONDecodeError as e:
         logger.error(e)
+        logger.error(traceback.format_exc())
         raise web.HTTPBadRequest(body=str(e))
+    except Exception as e:
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        raise
