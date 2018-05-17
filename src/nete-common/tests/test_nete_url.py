7ff60d134a3534c3fa7f998137636646c42a6dc7
Frank Ploss <mail@fqxp.de>
2018-05-17 08:46:07 +0200

    Use ConnectionType in NeteUrl

--- src/nete-common/tests/test_nete_url.py
+++ src/nete-common/tests/test_nete_url.py
@@ -1,12 +1,10 @@
-from nete.common.nete_url import NeteUrl, ConnectionTypeNotSupported
+from nete.common.nete_url import NeteUrl, ConnectionType, ConnectionTypeNotSupported
 import pytest
 
 
 def test_from_string_with_local_url():
     nete_url = NeteUrl.from_string('local:/tmp/socket')
-    assert nete_url.is_socket_url()
-    assert not nete_url.is_http_url()
-    assert not nete_url.is_ssh_url()
+    assert nete_url.connection_type == ConnectionType.UNIX
     assert nete_url.base_url is None
     assert nete_url.socket_path == '/tmp/socket'
 
@@ -14,18 +12,14 @@ def test_from_string_with_local_url():
 @pytest.mark.parametrize('scheme', ['http', 'https'])
 def test_from_string_with_http_url(scheme):
     nete_url = NeteUrl.from_string('{}://nete.io/path'.format(scheme))
-    assert not nete_url.is_socket_url()
-    assert nete_url.is_http_url()
-    assert not nete_url.is_ssh_url()
+    assert nete_url.connection_type == ConnectionType.TCP
     assert nete_url.base_url == '{}://nete.io/path'.format(scheme)
     assert nete_url.socket_path is None
 
 
 def test_from_string_with_ssh_url():
     nete_url = NeteUrl.from_string('http+ssh://user@example.org:2222')
-    assert nete_url.is_ssh_url()
-    assert not nete_url.is_socket_url()
-    assert not nete_url.is_http_url()
+    assert nete_url.connection_type == ConnectionType.SSH
     assert nete_url.base_url is None
     assert nete_url.socket_path is None
     assert nete_url.ssh_host == 'example.org'
